4c9485f6a665cd9bf3ccf4f938ec6e8c7b23fb26
jihyeog144 <jiheog144@gmail.com>
2025-04-30 13:09:23 +0900

    [Feat] addGameManager
    오브젝트나 게임 기능에 따른 게임매니저 구현

--- Assets/CJH_Work/GameManager.cs
+++ Assets/CJH_Work/GameManager.cs
@@ -7,8 +7,15 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] private Transform respawnPoint; // 리스폰 지점
+    [SerializeField] private Transform goalPoint; // 목표 지점 
+    [SerializeField] private Transform savePoint; // 세이브 지점
+    private bool Goal = false;
+
     private static GameManager instance; // 싱글톤 인스턴스
     public static GameManager Inst => instance; // 싱글톤 인스턴스
+
 
     public Player player { get; private set; } // 플레이어 관리
 
@@ -42,12 +49,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+
+    }
+
     // GameManager 초기화 (외부에서 호출)
     public void Initialize() //초기화 메서드 [GameManager.Inst.Initialize();]
     {
         Debug.Log("GameManager 초기화!"); // 게임 초기 세팅 등을 여기에 작성
 
         SetupPlayer();
+
+        savePoint = respawnPoint; // 초기 세이브 지점은 기본 리스폰 위치
     }
 
     // 플레이어를 세팅하는 메서드
@@ -61,6 +75,54 @@ public class GameManager : MonoBehaviour
             player = playerObj.AddComponent<Player>();
         }
 
+    }
+    public void OnPlayerDied()
+    {
+        Debug.Log("GameManager: 플레이어가 사망하였다!");
+
+        StartCoroutine(RespawnCoroutine());
+
+        // 여기서 게임 오버 처리, UI 호출, 씬 전환 등
+        // UIManager.Inst.ShowGameOver();
+        // SceneManager.LoadScene("GameOverScene");
+
+    }
+
+    public void OnPlayerReachedGoal()
+    {
+        if (Goal) return; // 중복 방지
+        Goal = true;
+
+        Debug.Log("GameManager: 목표 도달 처리 실행!");
+
+        // 여기서  게임 클리어 UI 띄우기, 씬 전환 등
+        // UIManager.Inst.ShowStageClear();
+        // SceneManager.LoadScene("NextStage");
+    }
+
+    public void OnPlayerReachedSave()
+    {
+        if (Goal) return; // 중복 방지
+        Goal = true;
+
+        Debug.Log("GameManager: 목표 도달 처리 실행!");
+
+        // 여기서  게임 클리어 UI 띄우기, 씬 전환 등
+        // UIManager.Inst.ShowStageClear();
+        // SceneManager.LoadScene("NextStage");
+    }
+
+    public void UpdateSavePoint(Transform newSavePoint)
+    {
+        savePoint = newSavePoint;
+        Debug.Log("새로운 세이브 포인트로 갱신됨: " + savePoint.position);
+    }
+
+    private IEnumerator RespawnCoroutine()
+    {
+        yield return new WaitForSeconds(1f); // 1초 후 리스폰 (원한다면 이 시간 조절 가능)
+
+        player.Respawn(savePoint.position); // 리스폰 포인트로 리스폰
     }
 
 }
